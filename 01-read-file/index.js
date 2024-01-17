586f2c815ad80f2082bc8eba253ce5fc24cf3000
Леха <znaxor74@mail.ru>
2024-01-17 15:31:17 +0300

    fix: change the srcript fot 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -7,5 +7,5 @@ const stream = fs.createReadStream(
   'utf-8',
 );
 stream.on('data', (chunk) => (data += chunk));
-stream.on('end', () => console.log('End', data));
+stream.on('end', () => console.log(data));
 stream.on('error', (error) => console.log('Error', error.message));
