60e3b9aca054b9bc8365cea1803ef5328d8024fa
unknown <znaxor74@mail.ru>
2024-01-17 21:55:40 +0300

    refactor: replace the 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,19 +3,17 @@ const fs = require('fs');
 const pathToFolder = path.resolve(__dirname, 'secret-folder');
 
 fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
-  if (err) console.log(err);
-  else {
-    files.forEach((file) => {
-      if (file.isFile()) {
-        fs.stat(path.join(pathToFolder, file.name), (err, stats) => {
-          if (err) return;
-          console.log(
-            `${path.basename(file.name, path.extname(file.name))} - ${path
-              .extname(file.name)
-              .slice(1)} - ${stats.size}`,
-          );
-        });
-      } else return;
+  if (err) return err;
+  files
+    .filter((file) => file.isFile())
+    .forEach((file) => {
+      fs.stat(path.join(pathToFolder, file.name), (err, stats) => {
+        if (err) return;
+        console.log(
+          `${path.basename(file.name, path.extname(file.name))} - ${path
+            .extname(file.name)
+            .slice(1)} - ${stats.size}`,
+        );
+      });
     });
-  }
 });
