ee0b85c0e10f1e45da6ee43d9d944d72b286e0f4
Леха <znaxor74@mail.ru>
2024-01-17 16:24:59 +0300

    fix: change the start with task 04..

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const fs = require('fs');
-fs.copyFile(
-  path.resolve(__dirname, 'files'),
-  path.resolve(__dirname, 'files-copy'),
+const currentDirectory = path.join(__dirname, '04-copy-directory');
+const copyDirectory = fs.mkdir(
+  path.join(__dirname, 'files-copy'),
+  {
+    recursive: true,
+  },
   (err) => {
     if (err) {
-      console.log('Error Found:', err);
+      return console.error(err);
     }
   },
 );
+return copyDirectory;
+
+// fs.copyFile(
+//   path.resolve(__dirname, 'files'),
+//   path.resolve(__dirname, 'files-copy'),
+//   (err) => {
+//     if (err) {
+//       console.log('Error Found:', err);
+//     }
+//   },
+// );
