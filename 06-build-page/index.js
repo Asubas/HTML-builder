d19ed7e0f3bbfbe6bd9c91fc36fd819f033b27b2
unknown <znaxor74@mail.ru>
2024-01-18 23:29:53 +0300

    fix: process the task 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,15 +5,21 @@ async function htmlBuilder() {
   const createDist = path.join(__dirname, 'project-dist');
   const htmlFIle = path.join(createDist, 'index.html');
   const cssFile = path.join(createDist, 'style.css');
+  const components = path.join(__dirname, 'components');
   try {
     const templateHtml = await fs.readFile(
       path.join(__dirname, 'template.html'),
       'UTF-8',
     );
-    console.log(templateHtml);
+    const regex = /\{\{[a-zA-Z]+\}\}/g;
+    const tags = templateHtml.match(regex);
+    // console.log(tags);
+    // console.log(templateHtml);
     await fs.mkdir(createDist, { recursive: true });
     await fs.writeFile(htmlFIle, '', { flag: 'w' });
     await fs.writeFile(cssFile, '', { flag: 'w' });
+    await fs.appendFile(htmlFIle, templateHtml.replace(regex, tags));
+    // htmlFIle.replace(tags, fs.readFile(path.join(components, )));
   } catch (err) {
     return console.log(err);
   }
