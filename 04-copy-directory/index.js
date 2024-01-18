f83a2676707f13ce9321e3a75d0e4db1ca902c52
Леха <znaxor74@mail.ru>
2024-01-18 09:45:37 +0300

    fix: add some function for 04-come-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,26 @@
 const path = require('path');
 const fs = require('fs');
 const currentDirectory = path.join(__dirname, 'files');
-const copyDirectory = fs.mkdir(
-  path.join(__dirname, 'files-copy'),
-  {
-    recursive: true,
-  },
-  (err) => {
-    if (err) return err;
-  },
-);
-// return copyDirectory;
+const copyDirectory = path.join(__dirname, 'files-copy');
+fs.rm(copyDirectory, { recursive: true }, (err) => {
+  if (err) return err;
+});
+
+fs.mkdir(copyDirectory, { recursive: true }, (err) => {
+  if (err) return err;
+});
 
 fs.readdir(currentDirectory, { withFileTypes: true }, (err, files) => {
   if (err) return err;
   files
     .filter((file) => file.isFile())
     .forEach((file) => {
-      fs.copyFile(file, path.join(copyDirectory, 'file'));
+      fs.copyFile(
+        path.join(currentDirectory, file.name),
+        path.join(copyDirectory, file.name),
+        (err) => {
+          if (err) return err;
+        },
+      );
     });
 });
